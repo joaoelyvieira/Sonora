2497cd11fea6b49472f12ea0355272a60a23ed8f
Joao Ely Vieira <jotaevieira@gmail.com>
2026-08-20 02:10:07 -0300

    Implementação da classe Musica.

---
+++ src/Musica.java
@@ -0,0 +1,55 @@
+public class Musica {
+    private static int contadorId = 0;
+
+    private int id;
+    private String titulo;
+    private String artista;
+    private int duracaoSegundos;
+    private int reproducoes;
+
+    public Musica(String titulo, String artista, int duracaoSegundos) {
+        contadorId++;
+        this.id = contadorId;
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracaoSegundos = duracaoSegundos;
+        this.reproducoes = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public int getDuracaoSegundos() {
+        return duracaoSegundos;
+    }
+
+    public int getReproducoes() {
+        return reproducoes;
+    }
+
+    public void reproduzir(){
+        reproducoes++;
+    }
+
+    public String getDuracaoFormatada() {
+        int minutos = duracaoSegundos / 60;
+        int segundos = duracaoSegundos % 60;
+        return String.format("%02d:%02d", minutos, segundos);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + titulo + " - " + artista
+                + " (" + getDuracaoFormatada() + ") - "
+                + reproducoes + " reproducoes";
+    }
+}
